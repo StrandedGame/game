0ba826bdf4d974c5af5884a744963899d77805c1
= <jochembrouwer@telfort.nl>
2015-08-12 16:52:47 +0200

    files are now used to be required

--- utilities/publish.lua
+++ utilities/publish.lua
@@ -1,26 +1,27 @@
-#!/usr/local/bin/lua
+return function()
 
-local DEPDIR = os.getenv("RBXDEPLOYMENTDIR")
-local DEPFILE = "assetid"
+	local DEPDIR = os.getenv("RBXDEPLOYMENTDIR")
+	local DEPFILE = "assetid"
 
-if not DEPDIR or not DEPFILE then
-	error("DEPDIR or DEPFILE must be set: is your RBXDEPLOYMENTDIR environment variable set?")
+	if not DEPDIR or not DEPFILE then
+		error("DEPDIR or DEPFILE must be set: is your RBXDEPLOYMENTDIR environment variable set?")
+	end
+
+	-- Assuming that mod is updated.. ?
+
+	local FILE = io.open("assetid.last", "r")
+	local NUM = FILE:read("*a")
+	print(NUM)
+	if not NUM:match("%d") then
+		error("is not an assetid: " .. NUM)
+	end
+
+	local DIR = DEPDIR
+	local PATH = DEPDIR.."/"..DEPFILE
+	file = io.open(PATH, "w+")
+	file:write(NUM)
+	file:close()
+	os.execute("cd "..DIR.. " && git add " .. DEPFILE .. "  && git commit -m update && git push")
+	print("Published succesfully")
+
 end
-
--- Assuming that mod is updated.. ?
-
-local FILE = io.open("assetid.last", "r")
-local NUM = FILE:read("*a")
-print(NUM)
-if not NUM:match("%d") then
-	error("is not an assetid: " .. NUM)
-end
-
-local DIR = DEPDIR
-local PATH = DEPDIR.."/"..DEPFILE
-file = io.open(PATH, "w+")
-file:write(NUM)
-file:close()
-os.execute("cd "..DIR.. " && git add " .. DEPFILE .. "  && git commit -m update && git push")
-print("Published succesfully")
-
--- utilities/upload.lua
+++ utilities/upload.lua
@@ -1,45 +1,46 @@
-#!/usr/local/bin/lua
+-- upload.lua: upload or update a model
 
-print("")
-
-url = require 'socket.url'
-
-local args = {...}
-local strgen
-
-local MODE = assert(args[1], "No mode choosen")
-local FILENAME
-if string.lower(MODE) == "new" then
-
-
-	FILENAME = assert(args[2], "No filename provided");
-	local MODNAME = assert(args[3], "No target filename provided");
-	local DESCRIPTION = assert(args[4], "No description provided");
-	strgen = "'http://www.roblox.com/Data/Upload.ashx?assetid=" .. "0".. "&type=model&name="..url.escape(MODNAME).."&description="..url.escape(DESCRIPTION).."&genreTypeId=1&ispublic=false&allowComments=false&groupId='"
-elseif string.lower(MODE) == "update" then
-	 FILENAME = assert(args[2], "No filename provided");
-	 local ASSETID = assert(args[3], "No assetId provided")
-	 strgen="'http://www.roblox.com/Data/Upload.ashx?assetid=" .. ASSETID .. "&type=Model'"
-else
-	error("Mode is unsupported")
-end 
+return function(...)
+	
+	url = require 'socket.url'
+	
+	local args = {...}
+	local strgen
+	
+	local MODE = assert(args[1], "No mode choosen")
+	local FILENAME
+	if string.lower(MODE) == "new" then
 
 
+		FILENAME = assert(args[2], "No filename provided");
+		local MODNAME = assert(args[3], "No target filename provided");
+		local DESCRIPTION = assert(args[4], "No description provided");
+		strgen = "'http://www.roblox.com/Data/Upload.ashx?assetid=" .. "0".. "&type=model&name="..url.escape(MODNAME).."&description="..url.escape(DESCRIPTION).."&genreTypeId=1&ispublic=false&allowComments=false&groupId='"
+	elseif string.lower(MODE) == "update" then
+		 FILENAME = assert(args[2], "No filename provided");
+		 local ASSETID = assert(args[3], "No assetId provided")
+		 strgen="'http://www.roblox.com/Data/Upload.ashx?assetid=" .. ASSETID .. "&type=Model'"
+	else
+		error("Mode is unsupported")
+	end 
 
-local url = strgen
 
-print(url)
 
-a = os.execute("./upload.sh " .. FILENAME .. " \"" .. url .. "\"")
+	local url = strgen
 
--- get last asset id from assetid.last file
+	print(url)
 
-file = io.open("./assetid.last", "r")
-if file then
-	local str = file:read("*a")
-	if str then
-		print("Last asset version id: " .. str)
-		os.exit()
+	a = os.execute("./utilities/upload.sh " .. FILENAME .. " \"" .. url .. "\"")
+
+	-- get last asset id from assetid.last file
+
+	file = io.open("./utilities/assetid.last", "r")
+	if file then
+		local str = file:read("*a")		
+		if str then
+			print("Last asset version id: " .. str)
+			return 
+		end
 	end
+	error("error reading asset file")
 end
-error("error reading asset file")
