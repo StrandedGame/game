929316de39de52c965add0e6c8c6d81aa8487568
= <jochembrouwer@telfort.nl>
2015-08-12 16:39:50 +0200

    added publish script

---
+++ utilities/publish.lua
@@ -0,0 +1,26 @@
+#!/usr/local/bin/lua
+
+local DEPDIR = os.getenv("RBXDEPLOYMENTDIR")
+local DEPFILE = "assetid"
+
+if not DEPDIR or not DEPFILE then
+	error("DEPDIR or DEPFILE must be set: is your RBXDEPLOYMENTDIR environment variable set?")
+end
+
+-- Assuming that mod is updated.. ?
+
+local FILE = io.open("assetid.last", "r")
+local NUM = FILE:read("*a")
+print(NUM)
+if not NUM:match("%d") then
+	error("is not an assetid: " .. NUM)
+end
+
+local DIR = DEPDIR
+local PATH = DEPDIR.."/"..DEPFILE
+file = io.open(PATH, "w+")
+file:write(NUM)
+file:close()
+os.execute("cd "..DIR.. " && git add " .. DEPFILE .. "  && git commit -m update && git push")
+print("Published succesfully")
+
