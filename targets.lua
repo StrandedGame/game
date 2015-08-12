d6c51df9bac9f427313446bfa925604a0e0e34f2
= <jobro13@users.noreply.github.com>
2015-08-13 01:04:27 +0200

    added targets interface but current setup doesnt allow this yet

---
+++ targets.lua
@@ -0,0 +1,17 @@
+--- Targets.lua: define roblox deployment targets
+
+local Targets = {}
+
+Targets.public = {
+	PlaceId =0 ;
+	SharedAssetId =0; 
+}
+
+Targets.test = {
+	PlaceId =281223367; 
+	SharedAssetId = 282804955;
+}
+
+return Targets
+
+
